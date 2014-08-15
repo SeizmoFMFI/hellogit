2c141031f69ed5f761beb0804db609852036b663
Filip Kubina <fkubina@gmail.com>
2014-08-15 16:51:41 +0200

    print active contributors

--- HelloGit/main.cpp
+++ HelloGit/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "global.h"
 using namespace std;
 
 int main() {
-	cout << "hello world" << endl;
+	cout << "hello git world" << endl<<endl;
+	cout << "add your name into the list of contributors and sync your changes" << endl;
+
+	users();
+
+	cout << endl << "press anything to exit" << endl;
+	cin.get();
 	return 0;
 }
---
+++ HelloGit/users.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "global.h"
+using namespace std;
+
+void users() {
+	cout << endl << "List of contributors:" << endl;
+	cout << "Filip" << endl;
+}
