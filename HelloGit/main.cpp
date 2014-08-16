4aeaecec51593e747bc77c7463f271f6d221f15b
Filip Kubina <fkubina@gmail.com>
2014-08-16 16:31:57 +0200

    added github issues

--- HelloGit/main.cpp
+++ HelloGit/main.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int main() {
-	cout << "hello git world" << endl<<endl;
-	cout << "add your name into the list of contributors and sync your changes" << endl;
+	cout << "hello git world" << endl << endl;
+
+	cout << "to learn using GitHub, please do following:" << endl;
+	cout << "1. edit source file and add your name into the list of contributors and sync your changes" << endl;
+	cout << "you are welcome to edit any part of this project :-)" << endl;
 
 	users();
 
-	cout << endl << "press anything to exit" << endl;
+	cout << endl;
+	cout << "2. go to github online repository and write/edit a issue (click 'issue' on the right panel" << endl;
+	cout << endl << "Program finished. Press anything to exit." << endl;
 	cin.get();
 	return 0;
 }
